f69f32b4714f3fb575e503259a4f994833e97a16
Forever699 <60701555+Forever699@users.noreply.github.com>
2020-11-04 08:46:45 +0530

    P2R Token.sol

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("YourTokenName", "YTN", 18) {
-        _mint(msg.sender, 10000000000 * (10 ** uint256(decimals())));
+    constructor () public ERC20Detailed("Poor2Rich", "P2R", 18) {
+        _mint(msg.sender, 1250000000 * (10 ** uint256(decimals())));
     }
 }
